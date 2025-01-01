68be3e9efe89824e57dc2e40df8a89ef519a285d
Ehimare Okoyomon <e.okoyomon@tum.de>
2025-01-01 21:19:20 +0100

    create model and training scripts

---
+++ models.py
@@ -0,0 +1,108 @@
+import torch
+import torch.nn as nn
+from torch_geometric.nn import GCNConv, ARMAConv
+
+# =============================================================================
+#                       Experiment models
+# =============================================================================
+
+# ARMA GNN class - Adaptation from paper.
+# J. B. Hansen, S. N. Anfinsen, and F. M. Bianchi,
+# “Power Flow Balancing With Decentralized Graph Neural Networks,”
+# IEEE Transactions on Power Systems, vol. 38, no. 3, pp. 2423–2433, May 2023,
+# doi: 10.1109/TPWRS.2022.3195301.
+class ARMA_GNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.leakyReLU = nn.LeakyReLU(negative_slope=0.2)
+        self.leakyReLU_small = nn.LeakyReLU(negative_slope=0.0)
+
+        # Pre-processing layers
+        self.predense1_node = nn.Linear(7, 64)
+        self.predense2_node = nn.Linear(64, 64)
+        self.predense1_edge = nn.Linear(4, 16)
+        self.predense2_edge = nn.Linear(16, 1)
+
+        # ARMA layers
+        self.arma = ARMAConv(64, 64, num_stacks=8, num_layers=4, shared_weights=False, act=self.leakyReLU, dropout=0.0, bias=True)
+
+        # Post-processing layer
+        self.postdense1 = nn.Linear(64, 64)
+        self.postdense2 = nn.Linear(64, 64)
+
+        # Output layer
+        self.readout = nn.Linear(64, 4)
+
+    def forward(self, data):
+        x = torch.nan_to_num(data.x, nan=0.0) # dim=(N, 7)
+        edge_index = data.edge_index # dim=(2, 2E)
+        # edge_attr = torch.nan_to_num(data.edge_attr, nan=0.0) # dim=(2E, 4)
+        
+        node_emb = self.leakyReLU(self.predense1_node(x))
+        node_emb = self.leakyReLU(self.predense2_node(node_emb))
+
+        # edge_emb = self.leakyReLU(self.predense1_edge(edge_attr))
+        # edge_emb = self.leakyReLU(self.predense2_edge(edge_emb))
+        # edge_emb = edge_emb.reshape((-1,))
+
+        # node_emb = self.arma(node_emb, edge_index, edge_weight=edge_emb)
+
+        # Edge_emb causes nan so do without for now.
+        node_emb = self.arma(node_emb, edge_index, edge_weight=None)
+
+        node_emb = self.leakyReLU(self.postdense1(node_emb))
+        node_emb = self.leakyReLU(self.postdense2(node_emb))
+
+        return self.readout(node_emb)
+
+
+# GCN class - custom GCN class w/ similar number of params as ARMA_GNN.
+class GCN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        # Constants
+        self.num_gcn_layers = 8
+        self.leakyReLU = nn.LeakyReLU(negative_slope=0.2)
+        self.leakyReLU_small = nn.LeakyReLU(negative_slope=0.005)
+
+        # Pre-processing layers
+        self.predense1_node = nn.Linear(7, 64)
+        self.predense2_node = nn.Linear(64, 64)
+        self.predense1_edge = nn.Linear(4, 16)
+        self.predense2_edge = nn.Linear(16, 1)
+
+        self.gcn_layers = nn.ModuleList([
+            GCNConv(64, 64, normalize=True) for i in range(self.num_gcn_layers)
+        ])
+
+        # Post-processing layer
+        self.postdense1 = nn.Linear(64, 64)
+        self.postdense2 = nn.Linear(64, 64)
+
+        # Output layer
+        self.readout = nn.Linear(64, 4)
+
+    def forward(self, data):
+        x = torch.nan_to_num(data.x, nan=0.0) # dim=(N, 7)
+        edge_index = data.edge_index # dim=(2, 2E)
+        edge_attr = torch.nan_to_num(data.edge_attr, nan=0.0) # dim=(2E, 4)
+        
+        node_emb = self.leakyReLU(self.predense1_node(x))
+        node_emb = self.leakyReLU(self.predense2_node(node_emb))
+
+        edge_emb = self.leakyReLU(self.predense1_edge(edge_attr))
+        # Using a leaky relu with too large of negative_slope can lead to
+        # sqrt of a negative number in GCNConv, so we use different leakyReLU
+        # in last step.
+        edge_emb = self.leakyReLU_small(self.predense2_edge(edge_emb))
+        edge_emb = edge_emb.reshape((-1,))
+
+        for i, layer in enumerate(self.gcn_layers):
+            node_emb = self.leakyReLU(layer(x=node_emb,
+                                            edge_index=edge_index,
+                                            edge_weight=edge_emb))
+
+        node_emb = self.leakyReLU(self.postdense1(node_emb))
+        node_emb = self.leakyReLU(self.postdense2(node_emb))
+
+        return self.readout(node_emb)
---
+++ train.py
@@ -0,0 +1,180 @@
+import numpy as np
+import torch
+import torch.nn as nn
+from torch_geometric.loader import DataLoader
+import matplotlib.pyplot as plt
+
+from collections import OrderedDict
+import time
+import os
+import sys
+
+from models import GCN, ARMA_GNN
+
+torch.manual_seed(12)
+
+# Tunables and other options
+P = OrderedDict([
+    ('model', 'gcn'), # Model: 'gcn', 'arma_gnn'
+    ('ouput_base_dir', 'outputs/2024-12-30_19:23:46/'),
+    ('training_grid', '1-LV-rural1--0-no_sw'),
+    ('testing_grid', '1-LV-rural3--0-no_sw'), # similar but diff grid
+    # ('testing_grid', '1-MV-comm--0-no_sw'), # very diff grid
+    ('epochs', 1000), # Number of epochs
+    ('learning_rate', 1e-3),
+    ('batch_size', 16),
+    ('early_stopping', True),
+    ('patience', 100), # Patience for early stopping
+    ('best_val_weights', True), # Return best validation weights
+    ('plot_loss', True),
+    ('save_model', False),
+    ('log_training', False),
+    ('test_model', True)
+])
+
+if P['log_training'] or P['save_model']:
+    SAVE_PATH = os.path.join('out',
+                             P['model'],
+                             time.strftime('%Y%m%d-%H%M%S'))
+    os.makedirs(SAVE_PATH, exist_ok=True)
+
+if P['log_training']:
+    logfile = os.path.join(SAVE_PATH, 'log.txt')
+    print('\nLogs will be saved in: ' + logfile)
+    sys.stdout = open(logfile, 'w')
+
+model_classes = {'gcn': GCN, 'arma_gnn': ARMA_GNN}
+
+##########################################################################
+# PYTORCH SETUP
+##########################################################################
+
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+)
+print(f"Training using {device}", flush=True)
+print(P, flush=True)
+
+##########################################################################
+# LOAD TRAINING AND VALIDATION DATASETS
+##########################################################################
+
+def get_pyg_graphs(ouput_base_dir, grid_type, split='train'):
+    dataset_path = os.path.join(ouput_base_dir, grid_type, split, f'dataset_{split}.pt')
+    pyg_dataset = torch.load(dataset_path, weights_only=False)
+    return pyg_dataset
+
+loader_train = DataLoader(get_pyg_graphs(P['ouput_base_dir'], P['training_grid'], split='train'),
+                          batch_size=P['batch_size'])
+loader_val = DataLoader(get_pyg_graphs(P['ouput_base_dir'], P['training_grid'], split='val'),
+                        batch_size=P['batch_size'])
+loader_test = DataLoader(get_pyg_graphs(P['ouput_base_dir'], P['testing_grid'], split='train'),
+                         batch_size=P['batch_size'])
+
+##########################################################################
+# MODEL SETUP AND TRAINING
+##########################################################################
+model = model_classes[P['model']]().to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+loss_fn = nn.MSELoss()
+
+restore_best_weights = P['best_val_weights']
+early_stopping = P['early_stopping']
+patience = P['patience']
+
+best_val_loss = np.Inf
+best_weights = None
+
+epochs = P['epochs']
+train_loss_vec = np.empty(epochs)
+train_loss_vec[:] = np.nan 
+val_loss_vec = np.empty(epochs)
+val_loss_vec[:] = np.nan
+start = time.time()
+for epoch in range(epochs):
+    model.train()
+    loss_train = 0
+    for batch_train in loader_train:
+        batch_train = batch_train.to(device)
+        optimizer.zero_grad()
+        pred = model(batch_train)
+        loss = loss_fn(pred, batch_train.y) # Average loss over the batch items.
+        loss.backward()
+        optimizer.step()
+        loss_train += loss.item()*batch_train.num_graphs
+    loss_train /= len(loader_train.dataset)
+
+    model.eval()
+    loss_val = 0
+    for batch_val in loader_val:
+        batch_val = batch_val.to(device)
+        pred = model(batch_val)
+        loss = loss_fn(pred, batch_val.y) # Average loss over the batch items.
+        loss_val += loss.item()*batch_val.num_graphs
+    loss_val /= len(loader_val.dataset)
+
+    if early_stopping == True or restore_best_weights == True:
+        if loss_val < best_val_loss:
+            wait = 0
+            best_weights = model.state_dict()
+            best_val_loss = loss_val
+        elif wait >= patience and early_stopping == True:
+            break
+        else:
+            wait += 1
+
+    train_loss_vec[epoch] = loss_train
+    val_loss_vec[epoch] = loss_val
+    if epoch % 10 == 9:
+        print('Epoch: {} Train Loss: {:.6f} Valid Loss: {:.6f}'
+                .format(epoch + 1, loss_train, loss_val), flush=True)
+
+train_time = time.time() - start
+print(f"Total Training Time (s): {train_time}", flush=True)
+
+if restore_best_weights == True:
+    model.load_state_dict(best_weights)
+
+if P['save_model'] == True:
+    torch.save(model.state_dict(), SAVE_PATH + '/model_weights')
+
+##########################################################################
+# PLOT LOSS (OPTIONAL)
+##########################################################################
+
+if P['plot_loss'] == True:
+    fig, ax = plt.subplots()
+    ax.plot(train_loss_vec, label = 'train loss')
+    ax.plot(val_loss_vec, label = 'val loss')
+    ax.set_xlabel('Epochs')
+    ax.set_ylabel('Loss')
+    ax.legend()
+    plt.show(block=True)
+
+##########################################################################
+# TESTING (OPTIONAL)
+##########################################################################
+
+def nrmse_loss(y_pred, y_real):
+    element_mse = torch.nn.MSELoss(reduction='none')(y_pred, y_real)
+    sqrt_n_mean = torch.sqrt(torch.mean(element_mse, dim=0)) # sqrt(Mean of N samples)
+    nrmse = torch.mean(sqrt_n_mean) # Normalized across features
+    return nrmse
+
+if P['test_model']:
+    nrmse_loss_fn = lambda pred, real: torch.sqrt(nn.MSELoss()(pred, real)) / torch.var(real, dim=1, keepdim=False)
+    model.eval()
+    loss_test = 0
+    for batch_test in loader_test:
+        batch_test = batch_test.to(device)
+        pred = model(batch_test)
+        loss = nrmse_loss(pred, batch_test.y) # Average loss over the batch items.
+        loss_test += loss.item()*batch_test.num_graphs
+    loss_test /= len(loader_test.dataset)
+
+    nrmse_val = loss_test
+    print(f"Test NRMSE: {nrmse_val}", flush=True)
